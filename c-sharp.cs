b03bc5fe83bc0e5106d0844deb035cb67efffec0
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2026-02-10 13:49:07 -0600

    Added additional files for fun

---
+++ c-sharp.cs
@@ -0,0 +1,12 @@
+// Example of a simple C# file with .cs extension
+using System;
+namespace HelloWorld
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           Console.WriteLine("Hello, World!");
+       }
+   }
+}
---
+++ javascript.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+
+firstName = "John";
+lastName = "Doe";
+
+price = 19.90;
+tax = 0.20;
+
+fullPrice = price + (price * tax);
